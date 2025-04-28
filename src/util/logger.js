94c74854e55c1a2a6467c8b9a5130a445373ad20
bablu@X-strategy <bablu@xstrategyservices.in>
2025-04-28 14:07:53 +0530

    fix: Removed file logging system

--- src/util/logger.js
+++ src/util/logger.js
@@ -2,13 +2,7 @@ import util from 'util'
 import 'winston-mongodb'
 import { createLogger, format, transports } from 'winston'
 import config from '../config/config.js'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-import path from 'path'
 import { blue, red, yellow, green, magenta } from 'colorette'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 const colorizeLevel = (level) => {
      switch (level) {
@@ -56,41 +50,6 @@ const consoleTransport = () => {
      return []
 }
 
-const fileLogFormat = format.printf((info) => {
-     const { level, message, timestamp, meta = {} } = info
-     const logMeta = {}
-     for (const [key, value] of Object.entries(meta)) {
-          if (value instanceof Error) {
-               logMeta[key] = {
-                    name: value.name,
-                    message: value.message,
-                    trace: value.stack || ''
-               }
-          } else {
-               logMeta[key] = value
-          }
-     }
-
-     const logData = {
-          level: level.toUpperCase(),
-          message,
-          timestamp,
-          meta: logMeta
-     }
-
-     return JSON.stringify(logData, null, 4)
-})
-
-const FileTransport = () => {
-     return [
-          new transports.File({
-               filename: path.join(__dirname, '../', '../', 'logs', `${config.ENV}.log`),
-               level: 'info',
-               format: format.combine(format.timestamp(), fileLogFormat)
-          })
-     ]
-}
-
 const MongodbTransport = () => {
      return [
           new transports.MongoDB({
@@ -107,5 +66,5 @@ export default createLogger({
      defaultMeta: {
           meta: {}
      },
-     transports: [...MongodbTransport(), ...FileTransport(), ...consoleTransport()]
+     transports: [...MongodbTransport(), ...consoleTransport()]
 })
